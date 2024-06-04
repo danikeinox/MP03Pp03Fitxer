fd83ccddfe2b3dab667a1998f07c90d6ceb54a2d
Daniel Cabrera <enamber01@gmail.com>
2024-06-04 16:41:58 +0200

    INITIAL PROJECT first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module daniel.cabrera.mp03pp03fitxer {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens daniel.cabrera.mp03pp03fitxer to javafx.fxml;
+    exports daniel.cabrera.mp03pp03fitxer;
+}
