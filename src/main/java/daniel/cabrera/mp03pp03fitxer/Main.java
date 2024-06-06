e64a979b9ab39a6d56b129b18e4cf63c686f0c43
Daniel Cabrera <enamber01@gmail.com>
2024-06-06 11:13:34 +0200

    REFRACTOR added implementations and initialized start method on Main.java

---
+++ src/main/java/daniel/cabrera/mp03pp03fitxer/Main.java
@@ -0,0 +1,41 @@
+package daniel.cabrera.mp03pp03fitxer;
+
+import daniel.Cabrera.Fitxers.Fitxers;
+import daniel.cabrera.mp03pp03fitxer.classes.Utils;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static String nomDir = ".data";
+    public static String rutaAdmin = "administradors.dat";
+    public static String rutaUsuari = "usuaris.dat";
+
+    // Inicialización de la aplicación y comprobación de la existencia de un administrador
+    @Override
+    public void start(Stage stage) throws Exception {
+        Fitxers f = new Fitxers();
+        if (!f.existeix(nomDir+"/"+rutaAdmin)) {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("formularis/user.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 500);
+            stage.setTitle("Control de Presència");
+            stage.setScene(scene);
+            stage.show();
+            Utils utils = new Utils();
+            utils.Alerta("No hi ha cap administrador a la base de dades, crea'n un.", "Error", "ERROR");
+            utils.FGeneric("formularis/admin.fxml", "Crear Administrador");
+        } else {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("formularis/user.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 500);
+            stage.setTitle("Control de Presència");
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
